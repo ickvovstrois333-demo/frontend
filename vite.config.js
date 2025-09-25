922584fb0d0ae29e23529c17388978cf0653be3f
ickvovstrois333-demo <ickvovstrois333@gmail.com>
2025-09-25 23:36:25 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // This ensures SPA routes fallback to index.html
+    historyApiFallback: true
+  },
+  build: {
+    rollupOptions: {
+      input: '/index.html'
+    }
+  }
 })
